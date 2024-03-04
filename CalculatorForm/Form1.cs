54b124dd94d112907931e078421b0a4dda788388
Kamal Huseynaliyev <kamalrh@code.edu.az>
2024-03-05 00:57:15 +0400

    Add project files.

---
+++ CalculatorForm/Form1.cs
@@ -0,0 +1,25 @@
+namespace CalculatorForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
